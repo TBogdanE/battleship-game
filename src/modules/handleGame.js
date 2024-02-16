ddcc79122685fc2d860522a56cd66da8478b88e9
tbogdane <bogdantoma2000@gmail.com>
2024-02-16 18:32:11 +0200

    handle player move and check if that position wasn't already hit

--- src/modules/handleGame.js
+++ src/modules/handleGame.js
@@ -1,9 +1,43 @@
 import { placePlayerBoats } from "./handlePlayer";
 import { placeComputerBoats } from "./handleComputer";
+import { checkContainsAny } from "./utils/checkContainsAny";
+import { gameSettings } from "./players";
 
-function startGame() {
-  placeComputerBoats();
-  placePlayerBoats();
+const computerBox = document.getElementById("computer-box");
+const playerBox = document.getElementById("player-box");
+
+async function startGame() {
+  //await placePlayerBoats();
+  await placeComputerBoats();
+  playerTurn();
 }
+
+function playerTurn() {
+  computerBox.addEventListener("click", clickHandler);
+}
+
+function clickHandler(event) {
+  const row = parseInt(event.target.getAttribute("row"));
+  const col = parseInt(event.target.getAttribute("col"));
+
+  playerClickHandler([row, col]);
+}
+
+function playerClickHandler(position) {
+  if (checkContainsAny([position], gameSettings.allHits)) {
+    console.log("already hit");
+    return;
+  }
+  gameSettings.allHits.push(position);
+  computerBox.removeEventListener("click", clickHandler);
+  computerTurn();
+}
+
+function computerTurn() {
+  //logic will go here
+  playerTurn();
+}
+
+function checkWin(player) {}
 
 export { startGame };
