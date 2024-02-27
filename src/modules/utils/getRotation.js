116e7df2b7cb729a9204a7fefbbb52c27c747491
Toma Bogdan <bogdantoma2000@gmail.com>
2024-02-27 18:58:44 +0200

    refactored getRotation()

--- src/modules/utils/getRotation.js
+++ src/modules/utils/getRotation.js
@@ -1,10 +1,18 @@
 const VERTICAL = "Vertical";
 const HORIZONTAL = "Horizontal";
 let ROTATION = VERTICAL;
+let rowStep = 0;
+let colStep = 0;
 
 const getRotation = (data = null) => {
-  if (data === null) return ROTATION;
-  ROTATION = data;
+  if (data !== null || data === VERTICAL || data === HORIZONTAL)
+    ROTATION = data;
+
+  if (ROTATION === VERTICAL) {
+    return [ROTATION, (rowStep = 1), (colStep = 0)];
+  } else if (ROTATION === HORIZONTAL) {
+    return [ROTATION, (rowStep = 0), (colStep = 1)];
+  }
   return;
 };
 
