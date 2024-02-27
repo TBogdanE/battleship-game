a099507432eeefa6fb079729cf05fd3a7dc88148
Toma Bogdan <bogdantoma2000@gmail.com>
2024-02-27 18:59:16 +0200

    remove comments

--- src/modules/utils/findElement.js
+++ src/modules/utils/findElement.js
@@ -1,6 +1,5 @@
 //searches the gameboard elements based on row and col
 export function findElementByRowCol(parent, row, col) {
   const selector = `[row="${row}"][col="${col}"]`;
-  console.log("parent", parent);
   return parent.querySelector(selector);
 }
